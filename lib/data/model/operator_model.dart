a5c88c763b503cdbc24860a7273444e7fd0a1aa9
Sadturtleman <rugsn12345@gmail.com>
2026-01-14 12:13:45 +0900

    feat: operator repo add

---
+++ lib/data/model/operator_model.dart
@@ -0,0 +1,15 @@
+class OperatorModel {
+  final String charId;
+  final String name;
+  final int rarity;
+  final String profession;
+  final String skinUrl;
+
+  OperatorModel({
+    required this.charId,
+    required this.name,
+    required this.rarity,
+    required this.profession,
+    required this.skinUrl
+  });
+}
